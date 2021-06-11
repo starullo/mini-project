efa5c48a87cbcf78053cca1df5627feddc3b161c
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-06-11 11:01:31 -0500

    ST: Created MovieController class

---
+++ mini/src/main/java/com/sam/mini/controller/MovieController.java
@@ -0,0 +1,81 @@
+package com.sam.mini.controller;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sam.mini.domain.Movie;
+import com.sam.mini.service.MovieService;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+	
+//	ATTRIBUTES
+	private MovieService service; 
+	
+//	CONSTRUCTORS
+	@Autowired
+	public MovieController(MovieService service) {
+		this.service = service; 
+	}
+	
+	// CREATE
+	@PostMapping("/create")
+	public ResponseEntity<Movie> createVehicle(@RequestBody Movie movieToAdd){
+		return new ResponseEntity<Movie>(this.service.createMovie(movieToAdd), HttpStatus.CREATED);
+	}
+	
+	// READ
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Movie>> getAllMovies(){
+		return new ResponseEntity(this.service.getAllMovies(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getOne/{id}")
+	public ResponseEntity<Movie> getVehicle(@PathVariable Long id) {
+		return new ResponseEntity(this.service.getMovie(id), HttpStatus.OK);
+	}
+	
+//	UPDATE
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updatedMovie){
+		this.service.updateMovie(id, updatedMovie);
+		return new ResponseEntity<Movie>(updatedMovie, HttpStatus.ACCEPTED);
+	}
+	
+	@PatchMapping("/patch/{id}")
+	public ResponseEntity<Movie> patchMovie(@PathParam("title") String newTitle, @PathParam("year") Integer newYear, @PathParam("rating") Double newRating, @PathParam("summary") String newSummary, @PathVariable Long id){
+
+		Movie toUpdate = this.service.getMovie(id);
+		
+		newTitle = newTitle == null ? toUpdate.getTitle() : newTitle;
+		newYear = newYear == null ? toUpdate.getYear() : newYear;
+		newRating = newRating == null ? toUpdate.getRating() : newRating;
+		newSummary = newSummary == null ? toUpdate.getSummary() : newSummary;
+		
+		return new ResponseEntity<Movie>(this.service.updateMovie(id, toUpdate), HttpStatus.OK);
+	}
+	
+	
+	// DELETE
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity deleteMovie(@PathVariable Long id) {
+		return this.service.deleteMovie(id) == true ? new ResponseEntity<Movie>(HttpStatus.OK) : new ResponseEntity<Movie>(HttpStatus.BAD_REQUEST);
+	}
+	
+
+}
