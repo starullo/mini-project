ccb5b635cbd11486a088e0fd25347da17a2dbef4
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-06-13 23:31:10 -0500

    ST: Finished integration testing and added a couple unit tests

--- mini/src/test/java/com/sam/mini/controller/MovieControllerIntegrationTest.java
+++ mini/src/test/java/com/sam/mini/controller/MovieControllerIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.sam.mini.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sam.mini.domain.Movie;
 
@@ -38,7 +41,7 @@ public class MovieControllerIntegrationTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(m1AsJson);
 		
-		Movie savedMovie = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie savedMovie = new Movie(2L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
 		
 		String savedMovieAsJson = this.mapper.writeValueAsString(savedMovie);
 		
@@ -49,6 +52,96 @@ public class MovieControllerIntegrationTest {
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchBody);
 		
 
+	}
+	
+	@Test
+	void testRead() throws Exception {
+		Movie m1 = new Movie("Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		String m1AsJson = this.mapper.writeValueAsString(m1);
+		
+		RequestBuilder mockRequestCreate = 
+				post("/movie/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(m1AsJson);
+		
+		Movie savedM1 = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		String savedM1AsJson = this.mapper.writeValueAsString(savedM1);
+		
+		RequestBuilder mockRequestOne = get("/movie/getOne/1");
+		
+		ResultMatcher matchStatus = status().is(200);
+		ResultMatcher matchBody = content().json(savedM1AsJson);
+		
+		this.mock.perform(mockRequestCreate);
+		this.mock.perform(mockRequestOne).andExpect(matchStatus).andExpect(matchBody);
+	}
+	
+	@Test
+	void testUpdate() throws Exception {
+		Movie m1 = new Movie("Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie updated = new Movie("Goodfellas", 1990, 10.0, "They're good and they're fellas and it's a movie hooray");
+		String m1AsJson = this.mapper.writeValueAsString(m1);
+		String updatedAsJson = this.mapper.writeValueAsString(updated);
+		
+		RequestBuilder mockRequest = 
+				post("/movie/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(m1AsJson);
+		Movie savedMovie = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		String savedMovieAsJson = this.mapper.writeValueAsString(savedMovie);
+		
+		RequestBuilder mockRequest2 = 
+				put("/movie/update/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(updatedAsJson);
+		Movie savedUpdated = new Movie(1L, "Goodfellas", 1990, 10.0, "They're good and they're fellas and it's a movie hooray");
+		String savedUpdatedAsJson = this.mapper.writeValueAsString(savedUpdated);
+
+		ResultMatcher matchStatus = status().is(202);
+		ResultMatcher matchBody = content().json(savedUpdatedAsJson);
+		this.mock.perform(mockRequest);
+		this.mock.perform(mockRequest2).andExpect(matchStatus).andExpect(matchBody);
+		
+	}
+	
+	@Test
+	void testDelete() throws Exception {
+		Movie m1 = new Movie("Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie m2 = new Movie("Peter Rabbit 2: The Runaway", 2021, 6.7, "Peter Rabbit causes trouble... BIG trouble... well not really I don't know whatever");
+		String m1AsJson = this.mapper.writeValueAsString(m1);
+		String m2AsJson = this.mapper.writeValueAsString(m2);
+		
+		
+		Movie savedOne = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie savedTwo = new Movie(2L, "Peter Rabbit 2: The Runaway", 2021, 6.7, "Peter Rabbit causes trouble... BIG trouble... well not really I don't know whatever");
+		String savedOneToJson = this.mapper.writeValueAsString(savedOne);
+		String savedTwoToJson = this.mapper.writeValueAsString(savedTwo);
+		Movie [] results = new Movie[1];
+		results[0] = savedTwo;
+		String resultsToJson = this.mapper.writeValueAsString(results);
+		
+		RequestBuilder mockRequest1 = 
+				post("/movie/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(m1AsJson);
+		
+		RequestBuilder mockRequest2 = 
+				post("/movie/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(m2AsJson);
+		
+		RequestBuilder mockDelete = delete("/movie/delete/1");
+		
+		RequestBuilder mockRequest3 = get("/movie/getAll");
+		
+		ResultMatcher matchStatus = status().is(202);
+		ResultMatcher matchBody = content().json(resultsToJson);
+		
+		this.mock.perform(mockRequest1);
+		this.mock.perform(mockRequest2);
+		this.mock.perform(mockRequest3).andExpect(matchBody);
+		
+		
 	}
 
 }
--- mini/src/test/java/com/sam/mini/service/MovieServiceUnitTest.java
+++ mini/src/test/java/com/sam/mini/service/MovieServiceUnitTest.java
@@ -1,12 +1,20 @@
 package com.sam.mini.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.sam.mini.domain.Movie;
 import com.sam.mini.repo.MovieRepo;
+
+import antlr.collections.List;
 
 @SpringBootTest
 public class MovieServiceUnitTest {
@@ -21,6 +29,29 @@ public class MovieServiceUnitTest {
 	void testCreateUnit() {
 		Movie m1 = new Movie("Goodfellas", 1990, 9.9, "Wise guys do unwise things");
 		Movie m1WithId = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		
+		Mockito.when(this.repo.save(m1)).thenReturn(m1WithId);
+		
+		assertEquals(this.service.createMovie(m1), m1WithId);
+		
+		Mockito.verify(this.repo, Mockito.times(1)).save(m1);
+	}
+	
+	@Test
+	void testRead() {
+		Movie m1 = new Movie("Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie m2 = new Movie("Peter Rabbit 2: The Runaway", 2021, 6.7, "Peter Rabbit causes trouble... BIG trouble... well not really I don't know whatever");
+		Movie m1WithId = new Movie(1L, "Goodfellas", 1990, 9.9, "Wise guys do unwise things");
+		Movie m2WithId = new Movie(2L, "Peter Rabbit 2: The Runaway", 2021, 6.7, "Peter Rabbit causes trouble... BIG trouble... well not really I don't know whatever");
+		ArrayList movies = new ArrayList<Movie>();
+		movies.add(m1WithId);
+		movies.add(m2WithId);
+		
+		Mockito.when(this.repo.findAll()).thenReturn(movies);
+		
+		assertEquals(this.service.getAllMovies(), movies);
+		
+		Mockito.verify(this.repo, Mockito.times(1)).findAll();
 	}
 	
 }
